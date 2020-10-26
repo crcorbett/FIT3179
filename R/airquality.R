f1a71fbb73a4a85b8ee96c2717c55202c4862965
Cooper Corbett <coopercorbett@gmail.com>
2020-10-26 15:10:02 +1100

    Update

--- R/airquality.R
+++ R/airquality.R
@@ -7,31 +7,33 @@ library('tidyverse')
 library('lubridate')
 library('jsonlite')
 
-# # Read in Air Quality data
-# airquality_data_2019 <- as_tibble(read_csv('data/raw/airquality/AQI-2019.csv')) %>% filter(City=='Melbourne')
-# airquality_data_2020 <- as_tibble(read_csv('data/raw/airquality/AQI-2020.csv')) %>% filter(City=='Melbourne')
-#  
-# airquality_data <- full_join(airquality_data_2019, airquality_data_2020)
-# 
-# airquality_data <- airquality_data %>% 
-#   select(-Country, -City) %>% 
-#   mutate(Day=day(Date), Month=month(Date)) %>%
-#   rename(Type = Specie)
-# airquality_data$Type <- as_factor(airquality_data$Type)
-# 
-# # Filter to certain environmental metrics
-# airquality_data <- airquality_data %>%
-#   filter(Type %in% c("co", "pm10", "no2", "o3", "so2","pm25")) %>%
-#   filter(Month == 7)
-# 
-# 
-# ggplot(airquality_data) +
-#   geom_boxplot(aes(x=Date, y=median, colour=Type)) +
-#   facet_wrap(~Type, scales="free")
-# 
-# ggplot(airquality_data) +
-#     geom_tile(aes(x=Date, y=Type, fill=median)) +
-#     facet_wrap(~year(Date), scales = "free")
+# Read in Air Quality data
+airquality_data_2019 <- as_tibble(read_csv('data/raw/airquality/AQI-2019.csv')) %>% filter(City=='Melbourne')
+airquality_data_2020 <- as_tibble(read_csv('data/raw/airquality/AQI-2020.csv')) %>% filter(City=='Melbourne')
+
+airquality_data <- full_join(airquality_data_2019, airquality_data_2020)
+
+airquality_data <- airquality_data %>%
+  select(-Country, -City) %>%
+  mutate(Day=day(Date), Month=month(Date)) %>%
+  rename(Type = Specie)
+airquality_data$Type <- as_factor(airquality_data$Type)
+
+# Filter to certain environmental metrics
+airquality_data <- airquality_data %>%
+  filter(Type %in% c("co", "pm10", "no2", "o3", "so2","pm25")) %>%
+  filter(Month %in% c(6, 7)) %>%
+  filter(Type=="no2") %>%
+  mutate(day=day(Date), month=month(Date), year=year(Date)) %>%
+  mutate(day_month=paste(day, month, sep="/"))
+
+
+ggplot(airquality_data) +
+  geom_boxplot(aes(x=Date, y=median, colour=Type)) +
+  facet_wrap(~Type, scales="free")
+
+ggplot(airquality_data) +
+    geom_tile(aes(x=factor(month(Date)), y=factor(year(Date)), fill=median, alpha=median))
 
 #
 #
@@ -41,15 +43,16 @@ airquality <- as_tibble(read_csv("data/raw/airquality/Melbourne - Microclimate S
 airquality$local_time <- ymd_hms(airquality$local_time)
 airquality$type <- as_factor(airquality$type)
 airquality <- airquality %>%
-  filter(month(airquality$local_time) %in% c(5,6,7)) %>%
   filter(type %in% c("PM2.5-EPA-1h-NOPK-EPA-24h", "PM10-EPA-1h-NOPK-EPA-24h"))
 
 airquality <- airquality %>% 
   group_by(site_id, type, date=date(local_time)) %>% 
-  summarise(med=median(value)) %>% 
-  filter(site_id=="arc1050") %>% 
-  filter(type %in% c("PM2.5", "PM10"))
+  mutate(mean=mean(value)) %>% 
+  filter(site_id=="arc1050")
 
+minmaxnormalise <- function(x, min, max){(x-min) /(max-min)}
 
-ggplot(airquality) +
-  geom_tile(aes(x=date, y=type, fill=med, group=type)) 
+airquality %>% 
+  mutate(normalised=rnorm(mean, mean = 0, sd = 1)) %>% 
+  ggplot() +
+  geom_tile(aes(x=date, y=type, fill=mean, group=type))
